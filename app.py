7f191117bbcc16570b904ced0f97528f7858f3c0
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-01-03 18:34:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -593,8 +593,7 @@ elif select=='read the language':
             my_file_name = "audio"
         tts.save(f"temp/{my_file_name}.mp3")
         return my_file_name, trans_text
-
-
+    
     display_output_text = st.checkbox("Display output text")
 
     if st.button("convert"):
