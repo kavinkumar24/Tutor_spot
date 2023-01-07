bd51fb211a62a538faf88fdef838a28b7a7224ed
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-01-07 16:33:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -666,5 +666,204 @@ elif select=='read the language':
             choose = reading.getPage(num)
             text = choose.extractText()
         st.write(text)
+        in_lang = st.selectbox(
+            " select the Input language ",
+            ("Afrikaans","Arabic","Bengali","Czech","Chinese","Danish","Dutch","English","Finnish","French","German","Greek","Hindi","Hungarian","Italian",  "Japanese", "Kannada","korean","Malayalam","Russian","Tamil","Telugu","Spanish","Urdu",),
+        )
+        if in_lang == "Afrikaans":
+            input_language = "af"
+        elif in_lang == "Arabic":
+            input_language = "ar"
+        elif in_lang =="Bengali":
+            input_language = "bn"
+        elif in_lang =="Czech":
+            input_language = "cs"
+        elif in_lang =="Chinese":
+            input_language = "zh-cn"
+        elif in_lang =="Danish":
+            input_language = "da"
+        elif in_lang =="Dutch":
+            input_language = "nl"
+        elif in_lang =="English":
+            input_language = "en"
+        elif in_lang =="Finnish":
+            input_language = "fi"
+        elif in_lang =="French":
+            input_language = "fr"
+        elif in_lang =="German":
+            input_language = "de"
+        elif in_lang =="Greek":
+            input_language = "el"
+        elif in_lang =="Hindi":
+            input_language = "hi"
+        elif in_lang =="Hungarian":
+            input_language = "hu"
+        elif in_lang =="Italian":
+            input_language = "it"
+        elif in_lang =="Japanese":
+            input_language = "ja"
+        elif in_lang =="Kannada":
+            input_language = "kn"
+        elif in_lang =="korean":
+            input_language = "ko"
+        elif in_lang =="Malayalam":
+            input_language = "ml"
+        elif in_lang =="Russian":
+            input_language = "ru"
+        elif in_lang =="Tamil":
+            input_language = "ta"
+        elif in_lang =="Telugu":
+            input_language = "te"
+        elif in_lang =="Spanish":
+            input_language = "es"
+        elif in_lang =="Urdu":
+            input_language = "ur"
+
+        out_lang = st.selectbox(
+            "Select the output language",
+            ("Afrikaans","Arabic","Bengali","Czech","Chinese","Danish","Dutch","English","Finnish","French","German","Greek","Hindi","Hungarian","Italian",  "Japanese", "Kannada","korean","Malayalam","Russian","Tamil","Telugu","Spanish","Urdu"),
+        )
+        if out_lang == "Afrikaans":
+            output_language = "af"
+            
+        elif out_lang == "Arabic":
+            output_language = "ar"
+            
+
+        elif out_lang =="Bengali":
+            output_language = "bn"
+        elif out_lang =="Czech":
+            output_language = "cs"
+        elif out_lang =="Chinese":
+            output_language = "zh-cn"
+        elif out_lang =="Danish":
+            output_language = "da"
+        elif out_lang =="Dutch":
+            output_language = "nl"
+        elif out_lang =="English":
+            output_language = "en"
+        elif out_lang =="Finnish":
+            output_language = "fi"
+        elif out_lang =="French":
+            output_language = "fr"
+        elif out_lang =="German":
+            output_language = "de"
+        elif out_lang =="Greek":
+            output_language = "el"
+        elif out_lang =="Hindi":
+            output_language = "hi"
+        elif out_lang =="Hungarian":
+            output_language = "hu"
+        elif out_lang =="Italian":
+            output_language = "it"
+        elif out_lang =="Japanese":
+            output_language = "ja"
+        elif out_lang =="Kannada":
+            output_language = "kn"
+        elif out_lang =="korean":
+            output_language = "ko"
+        elif out_lang =="Malayalam":
+            output_language = "ml"
+        elif out_lang =="Russian":
+            output_language = "ru"
+        elif out_lang =="Tamil":
+            output_language = "ta"
+        elif out_lang =="Telugu":
+            output_language = "te"
+        elif out_lang =="Spanish":
+            output_language = "es"
+        elif out_lang =="Urdu":
+            output_language = "ur"
+        
+
+        english_accent = st.selectbox(
+            "Select your english",
+            (
+                "Default",
+                "India",
+                "United Kingdom",
+                "United States",
+                "Canada",
+                "Australia",
+                "Ireland",
+                "South Africa",
+            ),
+        )
+
+        if english_accent == "Default":
+            tld = "com"
+        elif english_accent == "India": 
+            tld = "co.in"
+        elif english_accent == "United Kingdom":
+            tld = "co.uk"
+        elif english_accent == "United States":
+            tld = "com"
+        elif english_accent == "Canada":
+            tld = "ca"
+        elif english_accent == "Australia":
+            tld = "com.au"
+        elif english_accent == "Ireland":
+            tld = "ie"
+        elif english_accent == "South Africa":
+            tld = "co.za"
+
+
+        def text_to_speech(input_language, output_language, text, tld):
+            translation = translator.translate(text, src=input_language, dest=output_language)
+            trans_text = translation.text
+            tts = gTTS(trans_text, lang=output_language, tld=tld, slow=False)
+            try:
+                my_file_name = text[0:20]
+            except:
+                my_file_name = "audio"
+            tts.save(f"temp/{my_file_name}.mp3")
+            return my_file_name, trans_text
+
+
+        display_output_text = st.checkbox("Display output ")
+
+        if st.button("convert here"):
+            result, output_text = text_to_speech(input_language, output_language, text, tld)
+            audio_file = open(f"temp/{result}.mp3", "rb")
+            audio_bytes = audio_file.read()
+            st.markdown(f"## Your audio:")
+            st.audio(audio_bytes, format="audio/mp3", start_time=0)
+
+            if display_output_text:
+                st.markdown(f"## Output text:")
+                st.write(f" {output_text}")
+
+
+        def remove_files(n):
+            
+            mp3_files = glob.glob("temp/*mp3")
+            if len(mp3_files) != 0:
+                now = time.time()
+                n_days = n * 86400
+                for f in mp3_files:
+                    if os.stat(f).st_mtime < now - n_days:
+                        os.remove(f)
+                        print("Deleted ", f)
+
+
+        remove_files(7)
+        st.subheader("To see the pdf please give the realtive path:")
+        if st.checkbox("Show pdf "):
+            pdf_input=st.text_input("enter the relative path:")
+            pdf_path = ""
+            for k in pdf_input:
+                if k!='"':
+                    pdf_path=pdf_path+k
+                
+            try:
+                def show_pdf(pdf_path):
+                    with open(pdf_path,"rb") as f:
+                            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+                    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+                    st.markdown(pdf_display, unsafe_allow_html=True)
+                st.write(show_pdf(r"{}".format(pdf_path)))
+            except FileNotFoundError:
+                pass
+            
     except AttributeError:
         pass 
