62edf8fa87101f09f3af5c1ec8cd21b2ae3c1b10
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-01-08 12:54:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -816,7 +816,6 @@ elif select=='read the language':
             return my_file_name, trans_text
 
         display_output_text = st.checkbox("Display output ")
-
         if st.button("convert here"):
             result, output_text = text_to_speech(input_language, output_language, text, tld)
             audio_file = open(f"temp/{result}.mp3", "rb")
