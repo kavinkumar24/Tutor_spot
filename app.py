8a37cfde84335c5e9839fc74c8849d472c3c0671
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-01-12 21:43:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -790,7 +790,6 @@ elif select=='read the language':
         elif english_accent == "South Africa":
             tld = "co.za"
 
-
         def text_to_speech(input_language, output_language, text, tld):
             translation = translator.translate(text, src=input_language, dest=output_language)
             trans_text = translation.text
