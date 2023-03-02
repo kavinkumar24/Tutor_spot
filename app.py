949a614c9e404ac5d3d4f58df5e6425d32f29af1
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-03-02 22:15:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -140,6 +140,7 @@ elif select=="English Translator":
         st.write(translated_text.text)
         
         
+        
 elif select == "Contact":
     st.image("test.jpg")
     page_bg_img=f""" 
